9ee5c2586acf8f704f66903918ea09bf02669001
haythamdahri <haytham.dahri@gmail.com>
2019-10-22 18:12:41 +0100

    Initial commit
    Adding initial project files to the current commit

---
+++ src/main/java/com/challenge/backend/BackendCodingChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.challenge.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendCodingChallengeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendCodingChallengeApplication.class, args);
+    }
+
+}
